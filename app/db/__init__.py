7d7b67084b70bb45261e31b3143910c40980786a
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-05-18 15:08:16 +0100

    [IDDS-92] Add temp logging to view enums

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,3 +1,4 @@
+from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.postgresql import insert
 from sqlmodel import SQLModel, create_engine
@@ -79,6 +80,12 @@ def get_session():
     ]
 
     with CustomSessionLocal() as db_session:
+        print(
+            db_session.execute(
+                text("SELECT unnest(enum_range(NULL::publicbodyid))")
+            ).all()
+        )
+
         for proceeding in proceedings:
             stmt = (
                 insert(Proceeding)
