761c4f02cf7e85896b7859bc76ab01e6d9af2901
hevdevs <90092645+hevdevs@users.noreply.github.com>
2026-06-18 16:43:25 +0100

    [IDDS-117] update aws secret key lookup values

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -64,9 +64,9 @@ deploy_main() {
                 --set env.DB_USER="$DB_USER" \
                 --set env.DEPARTMENT_NAME="$DEPARTMENT_NAME" \
                 --set env.DEPARTMENT_URL="$DEPARTMENT_URL" \
-                --set env.AWS_SECRETS_GOV_NOTIFY_API_KEY="$GOV_NOTIFY_API_KEY" \
-                --set env.AWS_SECRETS_GOV_NOTIFY_APPLICATION_SUBMIT_TEMPLATE_ID="$GOV_NOTIFY_APPLICATION_SUBMIT_TEMPLATE_ID" \
-                --set env.AWS_SECRETS_GOV_NOTIFY_CALLBACK_BEARER_TOKEN="$GOV_NOTIFY_CALLBACK_BEARER_TOKEN" \
+                --set env.AWS_SECRETS_GOV_NOTIFY_API_KEY="gov-notify-api-key-$ENVIRONMENT" \
+                --set env.AWS_SECRETS_GOV_NOTIFY_APPLICATION_SUBMIT_TEMPLATE_ID="gov-notify-application-submit-template-id-$ENVIRONMENT" \
+                --set env.AWS_SECRETS_GOV_NOTIFY_CALLBACK_BEARER_TOKEN="gov-notify-callback-bearer-token-$ENVIRONMENT" \
                 --set env.CONTACT_EMAIL="$CONTACT_EMAIL" \
                 --set env.CONTACT_PHONE="$CONTACT_PHONE" \
                 --set env.NODE_ENV="$NODE_ENV" \
