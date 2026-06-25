1906d9e895b5b304a5ea3a812b950d723b999022
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-06-25 11:44:19 +0100

    [IDDS-315] Add whitelisting into helm deployments

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -16,6 +16,7 @@ deploy_branch() {
                 --install --wait --timeout 10m \
                 --namespace="${K8S_NAMESPACE}" \
                 --values ./deploy/infrastructure/helm/values/"$ENVIRONMENT".yaml \
+                --set ingress.allowList="$ALLOW_LIST" \
                 --set image.repository="$REGISTRY/$REPOSITORY" \
                 --set image.tag="$IMAGE_TAG" \
                 --set ingress.annotations."external-dns\.alpha\.kubernetes\.io/set-identifier"="$IDENTIFIER" \
