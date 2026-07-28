724bdc6e217b33a763f123a90b3a33a7ec258bad
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2026-07-28 16:27:06 +0100

    [IDDS-429] Update proceeding IDs and remove deprecated values from enum

--- bin/seed.py
+++ bin/seed.py
@@ -18,56 +18,6 @@ def seed():
     """
     proceedings = [
         {
-            "proceeding_id": "PC049",
-            "proceeding_name": "CAPA",
-            "proceeding_description": "CAPA",
-        },
-        {
-            "proceeding_id": "MN035",
-            "proceeding_name": "Clinical Negligence",
-            "proceeding_description": "Clinical Negligence",
-        },
-        {
-            "proceeding_id": "MN036",
-            "proceeding_name": "Death in Custody - Clinical Negligence",
-            "proceeding_description": "Death in Custody - Clinical Negligence",
-        },
-        {
-            "proceeding_id": "MH028",
-            "proceeding_name": "Mental Health",
-            "proceeding_description": "Mental Health",
-        },
-        {
-            "proceeding_id": "MH030",
-            "proceeding_name": "Death in Detention - Mental Health",
-            "proceeding_description": "Death in Detention - Mental Health",
-        },
-        {
-            "proceeding_id": "IQ001",
-            "proceeding_name": "Death in Custody",
-            "proceeding_description": "Death in Custody",
-        },
-        {
-            "proceeding_id": "IQ002",
-            "proceeding_name": "Inquest",
-            "proceeding_description": "Inquest",
-        },
-        {
-            "proceeding_id": "IQ003",
-            "proceeding_name": "Schedule 6 Town & Country Planning Act 1990",
-            "proceeding_description": "Schedule 6 Town & Country Planning Act 1990",
-        },
-        {
-            "proceeding_id": "IQ004",
-            "proceeding_name": "Public Inquiry s1 Inquiries Act 2005",
-            "proceeding_description": "Public Inquiry s1 Inquiries Act 2005",
-        },
-        {
-            "proceeding_id": "IQ010",
-            "proceeding_name": "S13 Coroner's Act 1988 - Public Law",
-            "proceeding_description": "S13 Coroner's Act 1988 - Public Law",
-        },
-        {
             "proceeding_id": "IQPC",
             "proceeding_name": "Death in police custody",
             "proceeding_description": "Death in police custody",
@@ -146,14 +96,35 @@ def seed():
 
     with CustomSessionLocal() as db_session:
         for proceeding in proceedings:
+            values = {
+                "proceeding_id": ProceedingId(proceeding["proceeding_id"]),
+                "proceeding_name": proceeding["proceeding_name"],
+                "proceeding_description": proceeding["proceeding_description"],
+                "category_of_law": "INQUESTS",
+                "certificate_type": "SUBSTANTIVE",
+                "level_of_service": "FULL_REPRESENTATION",
+                "matter_type": "INQUESTS",
+                "scope_limitation_heading": "FINAL_HEARING",
+                "scope_description": "This is the scope description",
+                "substantive_cost_limitation": 10000,
+            }
             stmt = (
                 insert(Proceeding)
-                .values(
-                    proceeding_id=ProceedingId(proceeding["proceeding_id"]),
-                    proceeding_name=proceeding["proceeding_name"],
-                    proceeding_description=proceeding["proceeding_description"],
+                .values(**values)
+                .on_conflict_do_update(
+                    index_elements=["proceeding_id"],
+                    set_={
+                        "proceeding_name": proceeding["proceeding_name"],
+                        "proceeding_description": proceeding["proceeding_description"],
+                        "category_of_law": "INQUESTS",
+                        "certificate_type": "SUBSTANTIVE",
+                        "level_of_service": "FULL_REPRESENTATION",
+                        "matter_type": "INQUESTS",
+                        "scope_limitation_heading": "FINAL_HEARING",
+                        "scope_description": "This is the scope description",
+                        "substantive_cost_limitation": 10000,
+                    },
                 )
-                .on_conflict_do_nothing(index_elements=["proceeding_id"])
             )
             db_session.exec(stmt)
 
