e88ade36c91b04993642428643d3fd8bc6e7ed11
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-08-07 15:32:32 +0100

    [IDDS-415] Add claim backlog scaffolding

--- app/domain/constants/report_csv_headers.py
+++ app/domain/constants/report_csv_headers.py
@@ -7,3 +7,15 @@ APPLICATION_BACKLOG_REPORT_HEADERS = [
     "Proceeding Code",
     "Matter Type",
 ]
+
+CLAIMS_BACKLOG_REPORT_HEADERS = [
+    "Claims Reference Number",
+    "Current Claims Status",
+    "Claim Submission Date",
+    "Office Name",
+    "Office Account Number (Firm Office Code)",
+    "Total 0% VAT claim value",
+    "Net total claim value",
+    "Gross total claim value",
+    "Claim type (POA or final bill)",
+]
