02210caa514c0e827e15c8afafd4e405862dce17
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-08-19 11:33:39 +0100

    [IDDS-652] Cleanup entra auth file

--- app/routers/dependencies/entra_auth.py
+++ app/routers/dependencies/entra_auth.py
@@ -28,37 +28,22 @@ def get_entra_auth_port() -> EntraAuthPort:
     )
 
 
-def verify_entra_token(
+async def verify_entra_token(
     credentials: Annotated[HTTPAuthorizationCredentials | None, Depends(_http_bearer)],
     entra_auth: Annotated[EntraAuthPort, Depends(get_entra_auth_port)],
-) -> None:
-    if credentials is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    user = entra_auth.verify_token(credentials.credentials)
-    set_entra_user_context(user.entra_object_id, user.name)
+) -> AuthenticatedUser:
+    return await _verify_entra_token_with_scopes(
+        credentials, entra_auth, _configured_entra_scopes()
+    )
 
 
 async def verify_entra_provider_token(
     credentials: Annotated[HTTPAuthorizationCredentials | None, Depends(_http_bearer)],
     entra_auth: Annotated[EntraAuthPort, Depends(get_entra_auth_port)],
 ) -> AuthenticatedUser:
-    if credentials is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    user = await run_in_threadpool(
-        entra_auth.verify_token, credentials.credentials, {"User.Provider"}
+    return await _verify_entra_token_with_scopes(
+        credentials, entra_auth, {"User.Provider"}
     )
-    set_entra_user_context(user.entra_object_id, user.name)
-    return user
 
 
 def get_current_provider_firm_code(
@@ -77,23 +62,24 @@ async def verify_entra_caseworker_token(
     credentials: Annotated[HTTPAuthorizationCredentials | None, Depends(_http_bearer)],
     entra_auth: Annotated[EntraAuthPort, Depends(get_entra_auth_port)],
 ) -> AuthenticatedUser:
-    if credentials is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    user = await run_in_threadpool(
-        entra_auth.verify_token, credentials.credentials, {"User.Caseworker"}
+    return await _verify_entra_token_with_scopes(
+        credentials, entra_auth, {"User.Caseworker"}
     )
-    set_entra_user_context(user.entra_object_id, user.name)
-    return user
 
 
-def verify_entra_provider_or_caseworker_token(
+async def verify_entra_provider_or_caseworker_token(
     credentials: Annotated[HTTPAuthorizationCredentials | None, Depends(_http_bearer)],
     entra_auth: Annotated[EntraAuthPort, Depends(get_entra_auth_port)],
+) -> AuthenticatedUser:
+    return await _verify_entra_token_with_scopes(
+        credentials, entra_auth, {"User.Provider", "User.Caseworker"}
+    )
+
+
+async def _verify_entra_token_with_scopes(
+    credentials: Annotated[HTTPAuthorizationCredentials | None, Depends(_http_bearer)],
+    entra_auth: Annotated[EntraAuthPort, Depends(get_entra_auth_port)],
+    required_scopes: set[str],
 ) -> AuthenticatedUser:
     if credentials is None:
         raise HTTPException(
@@ -102,6 +88,8 @@ def verify_entra_provider_or_caseworker_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    return entra_auth.verify_token(
-        credentials.credentials, {"User.Provider", "User.Caseworker"}
+    user = await run_in_threadpool(
+        entra_auth.verify_token, credentials.credentials, required_scopes
     )
+    set_entra_user_context(user.entra_object_id, user.name)
+    return user
