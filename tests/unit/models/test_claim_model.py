bfc3f73c86d69d19e84e69f63a39801e8c4a9907
Ben Fozard <ben.fozard@madetech.com>
2026-09-07 11:51:35 +0100

    [IDDS-472] PR review refactor

--- tests/unit/models/test_claim_model.py
+++
@@ -1,7 +0,0 @@
-from tests.unit.factories import create_base_claim
-
-
-def test_claim_keeps_internal_application_id():
-    claim = create_base_claim(application_id=12345)
-
-    assert claim.application_id == 12345
