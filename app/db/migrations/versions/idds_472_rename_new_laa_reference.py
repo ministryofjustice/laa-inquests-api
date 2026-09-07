9f1fe0d6e0cde88aa8e7132d093c7abebc287aa7
Ben Fozard <ben.fozard@madetech.com>
2026-09-07 10:36:14 +0100

    [IDDS-472] Migrate exisitng certificate created history events

--- app/db/migrations/versions/idds_472_rename_new_laa_reference.py
+++ app/db/migrations/versions/idds_472_rename_new_laa_reference.py
@@ -19,6 +19,28 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.alter_column("application", "new_laa_reference", new_column_name="laa_reference")
 
+    # Update existing history events with the new laa_reference value
+    op.execute(
+        """
+        UPDATE history_event
+        SET event_data = jsonb_set(to_jsonb(event_data), '{laa_reference}', to_jsonb(application.laa_reference))
+        FROM application
+        WHERE history_event.event_reference = 'CERTIFICATE_CREATED'
+        AND history_event.application_id = application.application_id
+        """
+    )
+
 
 def downgrade() -> None:
     op.alter_column("application", "laa_reference", new_column_name="new_laa_reference")
+
+    # Downgrade existing history events to use the old laa_reference value
+    op.execute(
+        """
+        UPDATE history_event
+        SET event_data = jsonb_set(to_jsonb(event_data), '{laa_reference}', to_jsonb(application.application_id))
+        FROM application
+        WHERE history_event.event_reference = 'CERTIFICATE_CREATED'
+        AND history_event.application_id = application.application_id
+        """
+    )
