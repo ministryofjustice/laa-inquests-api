ee27ac53aa3f518a95f720a5fa66d156fb064048
Gideon Gray <gideon.gray@justice.gov.uk>
2026-09-24 16:10:15 +0100

    [IDDS-885] Add test comment

--- app/auth/rbac.py
+++ app/auth/rbac.py
@@ -8,6 +8,7 @@ from app.ports.entra_auth_port import AuthenticatedUser
 from app.routers.dependencies import verify_entra_token
 
 
+# Test comment
 class Permission(str, Enum):
     APPLICATION_CREATE = "application:create"
     APPLICATION_READ = "application:read"
